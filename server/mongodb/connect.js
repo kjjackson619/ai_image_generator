e24f5516f44489a924a7bd6c97433dd4ad3f95e9
KJ Jackson <kjjackson619@yahoo.com>
2023-04-30 18:40:12 -0400

    mongodb connection

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = (url) => {
     mongoose.set('strictQuery', true);
 
-    mongoose.connect(url).then(() => console.log(err));
+    mongoose.connect(url).then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
 }
 
 export default connectDB;
